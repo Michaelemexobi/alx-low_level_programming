8989fdc49bf06b9050c06f889b9f4d49157921a5
Michaelemexobi <you@example.com>
2023-09-07 18:59:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
 #include <unistd.h>
-
 /**
  * main - This function prints a specific message.
  * Return: Always 1 (indicating success).
  */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
-
