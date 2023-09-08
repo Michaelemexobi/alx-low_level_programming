f5edde4088efb633cab1accbc62c333da177fb9e
Michaelemexobi <you@example.com>
2023-09-08 10:28:44 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of code
+ * Return: 0 Always successful
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
+
