78ef973f119e49cdeece577dc725cb524d38d2d6
Michaelemexobi <you@example.com>
2023-09-15 11:13:34 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+long number = 612852475143;
+long i;
+
+for (i = 2; i <= number; i++)
+{
+if (number % i == 0)
+{
+while (number % i == 0)
+{
+number = number / i;
+}
+}
+}
+
+printf("%ld\n", i - 1);
+
+return (0);
+}
+
