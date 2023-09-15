051c677d7b38b7a0e9fcc04b21302089f4b6ae56
Michaelemexobi <you@example.com>
2023-09-15 08:06:26 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper -prog checks for upper character
+ * @c:Variable text
+ * Return: 0 al ways successful
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
