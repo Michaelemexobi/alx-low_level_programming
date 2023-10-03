1558abc8137f5728c8c444529295f9f13ef4ea9c
Michaelemexobi <you@example.com>
2023-10-02 21:57:52 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program.
+ * @argc: number of arguments (unused).
+ * @argv: array of arguments.
+ *
+ * This program prints the name of the executable file. It takes an array of
+ * arguments, but the argument count is not used in this case.
+ *
+ * Return: Always returns 0.
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv);
+return (0);
+}
