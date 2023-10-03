7764f2517bab79df79735c383d8f734e562570e7
Michaelemexobi <you@example.com>
2023-10-02 22:49:58 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -12,7 +13,6 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-(void)
-printf("%s\n", argv);
+printf("%s\n", argv[0]);
 return (0);
 }
